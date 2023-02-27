03e7a88d49220b7e255fbdea24e29fa211de2f2e
Julia <124192611+JuliaOsipova11@users.noreply.github.com>
2023-02-27 15:33:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -49,4 +49,32 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-// 
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0;i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1); 
+//     }
+//     return array;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+//int[] myArray = CreateRandomArray(size, minValue, maxValue);
+
+// ShowArray(CreateRandomArray(size, minValue, maxValue));
